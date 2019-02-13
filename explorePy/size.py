95f27bc9fd0ad7512810b8cd9c65c55c9ff41651
Arzan Irani <arzan@Arzans-MacBook-Pro-TB.local>
2019-02-12 17:00:24 -0800

    Completed the implementation of size.py

--- explorePy/size.py
+++ explorePy/size.py
@@ -13,4 +13,17 @@ def size(x):
     returns:
     data frame - number of rows, number of columns and memory size of data frame
     """
-    return x.size()
+    # Calculate the number of rows and columns
+    number_of_rows = x.shape[0]
+    number_of_columns = x.shape[1]
+
+    # Get column-wise memory usuage and then
+    # sum it to get total memory used
+    total_bytes_used = sum(x.memory_usage())
+
+    # Create the results dataframe
+    result_df = pd.DataFrame({'rows': [number_of_rows],
+                              'columns' : [number_of_columns],
+                              'size_in_byes' : [total_bytes_used]})
+
+    return result_df
