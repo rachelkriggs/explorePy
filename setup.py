e0f223b29407ba227f42501cf16ccee1389f3231
Rachel K Riggs <43154927+rachelkriggs@users.noreply.github.com>
2019-02-15 15:50:12 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from distutils.core import setup
 
+setup(
+    name='explorePy',
+    version='0.1dev',
+    packages=['explorePy',],
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+      author = ['James Pushor', 'Rachel K. Riggs', 'Milos Milic', 'Arzan Irani'],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "pytest",
+        
+    ]
+)
