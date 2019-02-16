e91b19b2ad1d92996cd1e2320a9de5710781582a
Milos Milic <milicmil@mail.utoronto.ca>
2019-02-15 17:52:09 -0800

    fixed errors in tests and added error messages

--- tests/test_variable_summary.py
+++ tests/test_variable_summary.py
@@ -18,19 +18,19 @@ frames = pd.DataFrame({'index': [1, 2, 3, 4], 'letters': ["a","b", "c","d"],
 # input is data frame
 
 
-def test_correct_input():
-    assert isinstance(frames, pd.DataFrame)
+def test_correct_input(x):
+    assert isinstance(x, pd.DataFrame), "The input is not a data frame"
 
 #outputs are correct data frame dimensions
 
-def test_output_size():
-    assert frames.shape == (5,2)
+def test_output_size(x):
+    assert x.shape == (5,2), "The data frame should be 1 by 3"
 
 
 # test that variable-type is an string and count is an integer
 
-def test_column_type_integer_variable_type():
-    assert np.issubdtype(frames['variable_type'].dtype, np.string)
+def test_column_type_integer_variable_type(x):
+    assert np.issubdtype(x['variable_type'].dtype, np.object), "variable_type column should be an object type"
 
-def test_column_type_integer_variable_type():
-    assert np.issubdtype(frames['variable_type'].dtype, np.character)
+def test_column_type_integer_count(x):
+    assert np.issubdtype(x['count'].dtype, np.int64), "count column should be an integer type"
