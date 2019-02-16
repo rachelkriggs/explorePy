9d250c00a60b12bf9acd95237692541e9c654cdf
Rachel K Riggs <43154927+rachelkriggs@users.noreply.github.com>
2019-02-15 18:04:05 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1',
     packages=['explorePy',],
     license='LICENSE.txt',
-    long_description=open('README.txt').read(),
       author = ['James Pushor', 'Rachel K. Riggs', 'Milos Milic', 'Arzan Irani'],
     install_requires=[
         "numpy",
