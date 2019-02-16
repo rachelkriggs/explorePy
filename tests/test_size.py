8df4c725837098b60ca70a218e9d096e15e5fcc2
Milos Milic <milicmil@mail.utoronto.ca>
2019-02-15 17:53:06 -0800

    fixed errors in tests and added error messages

--- tests/test_size.py
+++ tests/test_size.py
@@ -18,22 +18,22 @@ frames = pd.DataFrame({'index': [1, 2, 3, 4], 'letters': ["a","b", "c","d"],
 # input is data frame
 
 
-def test_correct_input():
-    assert isinstance(frames, pd.DataFrame)
+def test_correct_input(x):
+    assert isinstance(x, pd.DataFrame), "The input is not a data frame"
 
 #outputs are correct data frame dimensions
 
-def test_output_size():
-    assert frames.shape == (1,3)
+def test_output_size(x):
+    assert x.shape == (1,3), "The data frame should be 1 by 3"
 
 
 # test that rows is an integer and columns is an integer and size in memory is float
 
-def test_column_type_integer_rows():
-    assert np.issubdtype(frames['letters'].dtype, np.character)
+def test_column_type_integer_rows(x):
+    assert np.issubdtype(x['letters'].dtype, np.object), "letters column should be an object type"
 
-def test_column_type_integer_columns():
-    assert np.issubdtype(frames['columns'].dtype, np.number)
+def test_column_type_integer_columns(x):
+    assert np.issubdtype(x['columns'].dtype, np.int64), "letters column should be an integer type"
 
-def test_column_type_float_size_in_memory():
-    assert np.issubdtype(frames['size_in_memory'].dtype, np.float)
+def test_column_type_float_size_in_memory(x):
+    assert np.issubdtype(x['size_in_memory'].dtype, np.float64),"size_in_memory column should be an integer type"
